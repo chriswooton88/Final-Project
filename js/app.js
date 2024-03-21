a431899c38d382ad8641ed76398d2a177512b435
chriswooton88 <chriswooton88@gmail.com>
2024-03-21 18:46:02 -0400

    Updated HTML, CSS, & JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const containerElement=window.document.querySelector(".browse-content")
-async function getData () {
-    const url = 'https://itunes.apple.com/search?country=us&media=podcast&term=popular';
+async function getData (type) {
+    const url = 'https://itunes.apple.com/search?country=us&media=podcast&term=' + type;
+    const containerElement=window.document.querySelector("."+type+"-content")
 const options = {
 	method: 'GET',
 		}
@@ -10,10 +10,11 @@ try {
 	const result = await response.json();
 	result.results.map((item) =>{
         console.log(item)
-        const {collectionName, artworkUrl100, trackViewUrl}=item;
+        const {collectionName, artworkUrl100, trackViewUrl, collectionViewUrl}=item;
         console.log(collectionName, artworkUrl100, trackViewUrl)
-        const itemElement=window.document.createElement("div")
-        const collectionNameElement=window.document.createElement("h2")
+        const itemElement=window.document.createElement("a")
+        itemElement.setAttribute("href", collectionViewUrl)
+        const collectionNameElement=window.document.createElement("h3")
         const imageElement=window.document.createElement("img")
         imageElement.setAttribute("src", artworkUrl100)
         collectionNameElement.textContent=collectionName
@@ -24,4 +25,5 @@ try {
 	console.error(error);
 }
 }
-getData()
+getData("popular")
+getData("trending")
